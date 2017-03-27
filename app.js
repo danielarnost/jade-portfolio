2e50f562683286199d7ad96184bdbca24caa94c8
danielarnost <danielarnost@utexaas.edu>
2017-03-27 12:40:38 -0500

    index and work, needs images more styling

--- app.js
+++ app.js
@@ -1,47 +1,38 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var nodemailer = require('nodemailer');
+'use strict'
+const express  = require("express"),
+  nodemailer   = require("nodemailer"),
+  bodyParser   = require("body-parser"),
+  cookieParser = require('cookie-parser'),
+  logger       = require("morgan"),
+  mongoose     = require("mongoose"),
+  path         = require('path');	  
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+const app = express();
+//middleware
+app.use('/static', express.static(__dirname + '/public'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//templates
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+
+
+
+//main routes
+app.get('/', function(req, res){
+	res.render('index');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/work', function(req, res){ 
+		res.render('work');	
 });
 
-module.exports = app;
+app.listen(3000, function() {
+	console.log("The frontend server is running on port 3000!");
+});
